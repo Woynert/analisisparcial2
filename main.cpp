c2a738f67376f266ec4c48163e00af40a567f3ec
woynert <whayner.porras.2020@upb.edu.co>
2021-11-08 16:09:46 -0500

    Fixed storage reference (Vector)

--- main.cpp
+++ main.cpp
@@ -1,17 +1,59 @@
-#include <iostream>
+//#include <iostream>
+#include <stdio.h>
 #include <vector>
 #include <string>
 using namespace std;
 
 
-void factorial(vector<int> fact, int n)
+int
+factorial (vector<int>* fact, int n)
 {
-	//check it already exists
-	
+	printf("Called\n");
+
+	//resize storage
+	if (fact->size () <= n)
+		fact->resize (n+1);
+
+	//check it exists
+	if ((*fact)[n] != 0)
+		return (*fact)[n];
+
+	//calculate
+	else
+	{
+		if (n <= 1)
+			(*fact)[n] = 1;
+		else
+			(*fact)[n] = n * factorial(fact, n-1);
+
+		return (*fact)[n];
+	}
+	return 0;
 }
 
-int main()
+int
+main()
 {
-	vector<int> fact = vector<int>();
+	vector<int>* fact = new vector<int>();
+	int n;
+
+	printf ("Factorial number calculator. Type -1 to exit.\n\n");
+
+	do
+	{
+		//input
+		printf (">> ");
+		scanf ("%i", &n);
+
+		if (n < 0)
+			break;
+
+		//compute
+		factorial(fact, n);
+
+		printf ("%i : Storage size: %i \n\n", (*fact)[n], fact->size());
+	}
+	while (n >= 0);
+
 	return 0;
 }
