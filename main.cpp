aab4ce1c9a1e18eeee1a858b05b96f5c433fa82b
woynert <whayner.porras.2020@upb.edu.co>
2021-11-08 15:25:41 -0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+void factorial(vector<int> fact, int n)
+{
+	//check it already exists
+	
+}
+
+int main()
+{
+	vector<int> fact = vector<int>();
+	return 0;
+}
